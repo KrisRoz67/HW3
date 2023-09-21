40f324326ee0adae7222801892534a87186c9e46
Kristina Rozanova <zolina.kristina@gmail.com>
2023-09-21 22:19:31 +0300

    First commit to new branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.concurrent.LinkedTransferQueue;
+
 public class Main {
+   public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Enter first number: ");
+        int firstInput = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int secondInput = scanner.nextInt();
+        System.out.println("The absolute value of difference is =" +getAbsoluteValue(firstInput,secondInput));
+        System.out.println("--------------------------------------");
+        System.out.println("Please enter your string to find first char of it :");
+        System.out.println("First char is : '" + getFirstCharOfString(scanner.next()) + "'");
+        System.out.println("--------------------------------------");
+        System.out.println("Please enter your string to find last char of it :");
+        String userString = scanner.next();
+        System.out.println("First char is : '" + getLastCharOfString(scanner.next()) + "'");
+        System.out.println("--------------------------------------");
+        System.out.println("Please enter your string to change order of chars :");
+        System.out.println("New string is: '" + changeOrderOfFirstChar(scanner.next()) + "'");
+    }
+    public static int getAbsoluteValue(int firstInput, int secondInput){
+        return Math.abs(firstInput-secondInput);
+    }
+    public static char getAskedCharOfString(String userString,int orderOfChar){
+        return userString.charAt(orderOfChar);
+    }
+    public static char getFirstCharOfString(String userString){
+        return getAskedCharOfString(userString,0);
+    }
+    public static char getLastCharOfString(String userString){
+        return getAskedCharOfString(userString,userString.length()-1);
+    }
+    public static String changeOrderOfFirstChar(String userString){
+        String newString = userString.substring(1,userString.length()-1);
+        return getLastCharOfString(userString)+
+                newString+getFirstCharOfString(userString);
     }
 }
