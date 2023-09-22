8d6c2945e39b6649cf4947e7b5cc39689c75adb3
Kristina Rozanova <zolina.kristina@gmail.com>
2023-09-22 12:13:10 +0300

    Code fix added

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;
 public class Main {
+
    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        Main main = new Main();
         System.out.println("Enter first number: ");
         int firstInput = scanner.nextInt();
         System.out.println("Enter second number: ");
         int secondInput = scanner.nextInt();
-        System.out.println("The absolute value of difference is =" +getAbsoluteValue(firstInput,secondInput));
+        System.out.println("The absolute value of difference is =" +main.getAbsoluteValue(firstInput,secondInput));
         System.out.println("--------------------------------------");
         System.out.println("Please enter your string to find first char of it :");
-        System.out.println("First char is : '" + getFirstCharOfString(scanner.next()) + "'");
+        System.out.println("First char is : '" + main.getFirstCharOfString(scanner.next()) + "'");
         System.out.println("--------------------------------------");
         System.out.println("Please enter your string to find last char of it :");
-        System.out.println("Last char is : '" + getLastCharOfString(scanner.next()) + "'");
+        System.out.println("Last char is : '" + main.getLastCharOfString(scanner.next()) + "'");
         System.out.println("--------------------------------------");
         System.out.println("Please enter your string to change order of chars :");
-        System.out.println("New string is: '" + changeOrderOfFirstChar(scanner.next()) + "'");
+        System.out.println("New string is: '" + main.swapFirstAndLastCharacter(scanner.next()) + "'");
     }
-    public static int getAbsoluteValue(int firstInput, int secondInput){
-        return Math.abs(firstInput-secondInput);
+    public int getAbsoluteValue(int firstInput, int secondInput){ return Math.abs(firstInput-secondInput);
     }
-    public static char getAskedCharOfString(String userString,int orderOfChar){
-        return userString.charAt(orderOfChar);
+    /**in case you need find any char **/
+    public char getAskedCharOfString(String userString,int orderOfChar){ return userString.charAt(orderOfChar);
     }
-    public static char getFirstCharOfString(String userString){
-        return getAskedCharOfString(userString,0);
+    public char getFirstCharOfString(String userString){ return getAskedCharOfString(userString,0);
     }
-    public static char getLastCharOfString(String userString){
-        return getAskedCharOfString(userString,userString.length()-1);
+    public char getLastCharOfString(String userString){ return getAskedCharOfString(userString,userString.length()-1);
     }
-    public static String changeOrderOfFirstChar(String userString){
+    public  String swapFirstAndLastCharacter(String userString){
         String newString = userString.substring(1,userString.length()-1);
-        return getLastCharOfString(userString)+
-                newString+getFirstCharOfString(userString);
+        return getLastCharOfString(userString)+ newString+getFirstCharOfString(userString);
     }
 }
